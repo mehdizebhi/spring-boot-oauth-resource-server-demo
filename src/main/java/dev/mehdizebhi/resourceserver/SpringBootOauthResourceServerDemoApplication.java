fd1007255d60b8230ae07427f212eabd0443c907
mehdizebhi <mehdizebhi@yahoo.com>
2024-03-15 17:45:01 +0330

    initialize git

---
+++ src/main/java/dev/mehdizebhi/resourceserver/SpringBootOauthResourceServerDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.mehdizebhi.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauthResourceServerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOauthResourceServerDemoApplication.class, args);
+    }
+
+}
