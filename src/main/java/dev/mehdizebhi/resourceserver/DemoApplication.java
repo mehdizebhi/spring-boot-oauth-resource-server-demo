37c35fd4ac49a96db7ffff2aff08f49068d169f6
mehdizebhi <mehdizebhi@yahoo.com>
2024-03-15 20:57:22 +0330

    update / update main class name

--- src/main/java/dev/mehdizebhi/resourceserver/DemoApplication.java
+++ src/main/java/dev/mehdizebhi/resourceserver/DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootOauthResourceServerDemoApplication {
+public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootOauthResourceServerDemoApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 
 }
