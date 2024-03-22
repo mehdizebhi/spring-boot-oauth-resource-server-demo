dc519d7c5d8749905200ff771fe452725e0d39aa
mehdizebhi <mehdizebhi@yahoo.com>
2024-03-22 19:45:21 +0330

    feature / add RepositoryRestConfigurer for Spring Data Rest

--- src/main/java/dev/mehdizebhi/resourceserver/DemoApplication.java
+++ src/main/java/dev/mehdizebhi/resourceserver/DemoApplication.java
@@ -2,6 +2,10 @@ package dev.mehdizebhi.resourceserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +14,12 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Component
+    class CustomizedRestMvcConfiguration implements RepositoryRestConfigurer {
+
+        @Override
+        public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+            config.exposeIdsFor(Todo.class);
+        }
+    }
 }
